bc2dd20efacd8870ebe99f7a13148ccde0c96f1d
Prashant Chaudhary <prashant@codeyogi.io>
2022-05-09 19:29:35 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="bg-red-500">Hello world!</div>;
+}
+
+export default App;
+
+// npm - dependency managment tool
